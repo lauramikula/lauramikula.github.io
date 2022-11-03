670de1f95fdb03deae282c553b379f31886bcd64
lauramikula <mikula.lora@gmail.com>
2022-11-02 21:45:37 -1000

    common header and footer for all pages

--- en/header-footer.js
+++ en/header-footer.js
@@ -14,7 +14,7 @@ template.innerHTML = `
   <div class="menu-wrap">
     <nav class="menu">
       <ul class="clearfix">
-        <li><a href="/index.html#about-me">About Me!!!</a></li>
+        <li><a href="/index.html#about-me">About Me</a></li>
         <li><a href="/en/academics.html">Academics</a></li>
         <li><a href="/en/publications.html">Publications</a></li>
         <li><a href="/en/media.html">Media</a></li>
@@ -28,9 +28,9 @@ template.innerHTML = `
   <div class="inner">
     <ul class="icons">
       <li><a href="https://scholar.google.fr/citations?user=SbpVBVQAAAAJ&hl=fr" class="icon ai-google-scholar"><span class="label">Google Scholar</span></a></li>
-      <li><a href="https://www.researchgate.net/profile/Laura_Mikula" class="icon ai-researchgate"><span class="label">ResearchGate</span></a></li>
+      <li><a href="https://researchgate.net/profile/Laura-Mikula-2" class="icon ai-researchgate"><span class="label">ResearchGate</span></a></li>
       <li><a href="https://github.com/lauramikula" class="icon fa-github"><span class="label">Github</span></a></li>
-      <li><a href="https://www.linkedin.com/in/lauramikula/" class="icon fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+      <li><a href="https://linkedin.com/in/lauramikula" class="icon fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
       <li><a href="https://twitter.com/MikulaLaura" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
       <li><a href="mailto:mikulal@yorku.ca" class="icon fas fa-at"><span class="label">Email</span></a></li>
     </ul>
